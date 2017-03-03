0fb9398affee1acef77b6960d128546a650bcfd5
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-03 08:46:42 -0800

    pass test save Store.

--- src/Store.php
+++ src/Store.php
@@ -30,22 +30,22 @@
         // Crud function
         function save()
         {
-            // $GLOBALS['DB']->exec("INSERT INTO stores (name) VALUES ('{$this->name}');");
-            // $this->id = (int) $GLOBALS['DB']->LastInsertId();
+            $GLOBALS['DB']->exec("INSERT INTO stores (name) VALUES ('{$this->name}');");
+            $this->id = (int) $GLOBALS['DB']->LastInsertId();
         }
 
         static function getAll()
         {
-            // $stores = array();
-            // $query = $GLOBALS['DB']->query("SELECT * FROM stores;");
-            // foreach( $query as $store)
-            // {
-            //     $store_name = $store['name'];
-            //     $store_id = $store['id'];
-            //     $re_store = new Store($store_name, $store_id);
-            //     array_push($stores, $re_store);
-            // }
-            // return $stores;
+            $stores = array();
+            $query = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            foreach( $query as $store)
+            {
+                $store_name = $store['name'];
+                $store_id = $store['id'];
+                $re_store = new Store($store_name, $store_id);
+                array_push($stores, $re_store);
+            }
+            return $stores;
         }
     }
 
