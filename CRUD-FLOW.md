daa0b338c79482d2fe3b6310b3f9262e178e8c5b
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-03 08:04:26 -0800

    edit basic app structure.

--- CRUD-FLOW.md
+++ CRUD-FLOW.md
@@ -3,4 +3,4 @@
 | Objects | Create | Read | Update |Delete |
 |----|------|-----|----|----|
 | Store | index form->post to store/{id} | stores/   store/{id} - this will have all the brands for store. | store/{id} | store/{id} store_termination |
-| Brand | index form->post to brand/{id} | brands/ brand/{id} - will show stores that cary it. | brand/{id}| brand/{id}|
+| Brand | index form->post to brand/{id} | brands/ brand/{id} - will show stores that cary it. | | |
--- app/app.php
+++ app/app.php
@@ -30,55 +30,76 @@
         return $app['twig']->render('index.html.twig');
 
     });
+
     //Create store
     $app->post('/stores/create', function() use ($app) {
 
         return $app->redirect('/stores');
 
     });
+
     // Read stores
     $app->get('/stores', function() use ($app){
 
+        return $app['twig']->render('stores.html.twig');
+
     });
+
     // Read store (singular)
     $app->get('/stores/{id}', function($id) use ($app){
 
+        return $app['twig']->render('store.html.twig');
+
     });
+
     // Update store
     $app->patch('/stores/update', function() use ($app){
 
+        return $app->redirect('/stores/{id}');
+
     });
+
     // Delete stores
     $app->delete('/stores/delete', function() use ($app){
 
         return $app->redirect('/stores');
 
     });
+
     // Delete store (singular)
     $app->delete('/stores/delete_singular', function() use ($app){
 
-        return $app->redirect('/stores/');
+        return $app->redirect('/stores/{id}');
 
     });
+
     //Create brand
     $app->post('/brands/create', function () use ($app){
 
         return $app->redirect('/brands');
 
     });
+
     // Read brands
     $app->get('/brands', function () use ($app) {
 
         return $app->render('brands.html.twig');
 
     });
+
     // Read brand (singular)
     $app->get('/brands/{id}', function($id) use ($app) {
 
         return $app->render('brand.html.twig');
-        
+
     });
+
     // Match brand to a store
+    $app->post('/brands/match', function() use ($app) {
+
+        return $app->redirect('/brands/{id}');
+
+    });
 
     return $app;
 
