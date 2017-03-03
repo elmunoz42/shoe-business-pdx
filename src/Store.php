fac451329531199840a8b6064bc157d2484bbebd
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-03 08:59:17 -0800

    pass test update Store.

--- src/Store.php
+++ src/Store.php
@@ -55,7 +55,7 @@
 
         function update($new_name)
         {
-            // $GLOBALS['DB']->exec("UPDATE stores SET name = '{$new_name}' WHERE id={$this->getId()};");
+            $GLOBALS['DB']->exec("UPDATE stores SET name = '{$new_name}' WHERE id={$this->getId()};");
         }
     }
 
