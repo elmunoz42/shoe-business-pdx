6bf4ac365ac331fa357b3e1a8071460a8fa3ac77
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-03-03 03:25:35 -0800

    add initial files.

---
+++ app/app.php
@@ -0,0 +1,35 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Store.php';
+    require_once __DIR__.'/../src/Brand.php';
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+
+    $server = 'mysql:host=localhost:8889;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get('/', function() use($app) {
+
+        return $app['twig']->render('index.html.twig');
+        
+    });
+
+    return $app;
+
+?>
